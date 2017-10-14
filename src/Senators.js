9745180ce8549b7191afb528163a9f7e85173b59
Danny Warren <dannydwarren@gmail.com>
2017-10-14 13:28:00 -0600

    Completed Solution - Search by First or Last Name and by State

--- src/Senators.js
+++ src/Senators.js
@@ -3,18 +3,66 @@ import SenatorsData from './data/senators.json'
 import SenatorComponent from './SenatorComponent'
 
 class Senators extends Component {
+
+    state = {
+        nameQuery: '',
+        stateQuery: ''
+    }
+
+    getByFilters(party) {
+        console.log(this.state.nameQuery)
+        console.log(this.state.stateQuery)
+
+        return SenatorsData.filter((senator) => {
+            return senator.party === party
+        }).filter((senator) => {
+            return senator.state.toLowerCase().includes(this.state.stateQuery.toLowerCase())
+        }).filter((senator) => {
+            return senator.person.lastname.toLowerCase().includes(this.state.nameQuery.toLowerCase())
+                || senator.person.firstname.toLowerCase().includes(this.state.nameQuery.toLowerCase())
+        })
+    }
+
+    getDemacrats(){
+        return this.getByFilters("Democrat")
+            .map((senator, i)=> {
+                return (
+                    <SenatorComponent key={i} senatorData={senator} />
+                )
+            })
+    }
+
+    getRepublicans(){
+        return this.getByFilters("Republican")
+            .map((senator, i)=> {
+                return (
+                    <SenatorComponent key={i} senatorData={senator} />
+                )
+            })
+    }
+
     render() {
         return (
             <div>
                 <div>
-                    <h1>Democrats</h1>
-                {SenatorsData.filter((senator) => {
-                    return senator.party === 'Democrat'
-                }).map((senator)=> {
-                        return (
-                            <SenatorComponent senatorData={senator} />
-                        )})}</div>
+                    <h4>Search by first or last name here:</h4>
+                    <input type="search" placeholder="Name Here"
+                           onChange={(event)=> this.setState({nameQuery: event.target.value})}/>
                 </div>
+                <div>
+                    <h4>Search by state here:</h4>
+                    <input type="search" placeholder="Name Here"
+                        onChange={(event)=> this.setState({stateQuery: event.target.value})}/>
+                </div>
+                <div>
+                    <h1>Democrats</h1>
+                    {this.getDemacrats()}
+                </div>
+                <div>
+                    <h1>Republicans</h1>
+                    {this.getRepublicans()}
+                </div>
+            </div>
         )
     }
 }  
